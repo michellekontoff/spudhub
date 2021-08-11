7c1b6e7d3bd344b5019c60f32c47f8e285828c6c
michellekontoff <mlkontoff@gmail.com>
2021-08-11 19:06:03 -0400

    made/started state plan

--- documentation/state-plan.js
+++ documentation/state-plan.js
@@ -6,46 +6,46 @@
         ---the cookie gets deleted and clears the store */
 
 store =
-{
-    shopping_cart: {
-        1: {
-            id: 1,
-            unit_price: 14.17,
-            quantity: 3
+    {
+        shopping_cart: {
+            1: {
+                id: 1,
+                unit_price: 14.17,
+                quantity: 3
+            },
+            6: {
+                id: 6, // this is the product id
+                unit_price: 11.11,
+                quantity: 4
+            }
         },
-        6: {
-            id: 6, // this is the product id
-            unit_price: 11.11,
-            quantity: 4
-        }
-    },
 
-    products: {
-        1: {
-            id: 1,
-            user_id: 3,
-            name: 'potato',
-            description: 'yum yum',
-            quantity: 1,
-            price: 100,
+        products: {
+            1: {
+                id: 1,
+                user_id: 3,
+                name: 'potato',
+                description: 'yum yum',
+                quantity: 1,
+                price: 100,
+            },
+            2: {
+                id: 2,
+                user_id: 7,
+                name: 'also potato',
+                description: 'not as yum',
+                quantity: 99,
+                price: -40.90,
+            }
         },
-        2: {
-            id: 2,
-            user_id: 7,
-            name: 'also potato',
-            description: 'not as yum',
-            quantity: 99,
-            price: -40.90,
-        }
-    },
 
-    session: {
-        user: {
-            id: 1,
-            username: 'Username',
-            address: 'end of the universe',
+        session: {
+            user: {
+                id: 1,
+                username: 'Username',
+                address: 'end of the universe',
+            }
         }
     }
-}
 
 console.log(store)
