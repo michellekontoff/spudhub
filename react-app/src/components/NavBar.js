da27f40f810db88b43e8b5b5437d894d8262f7d1
steffano <steffano.espinoza10@gmail.com>
2021-08-17 14:15:23 -0700

    sidebar, logout butn, and cart css added

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -9,52 +9,58 @@ import './NavBar.css'
 const NavBar = () => {
 
   const user = useSelector(state => state.session.user);
-  const [optionsOn, setOptionsOn] = useState(false)
+  const [optionsOn, setOptionsOn] = useState(false);
+  const [cart, setCart] = useState(false);
 
   return (
     <>
     <nav>
       <div className='nav_container'>
         <div className='nav_logo'>
+          <div>
+            <button className='nav_sidebar_icons' onClick={()=> setOptionsOn(!optionsOn)}>
+              <i className="fas fa-bars" />
+            </button>
+          </div>
           <NavLink to='/' exact={true} activeClassName='active'>
             SpudHub
           </NavLink>
         </div>
-        <div>
-            <button onClick={()=> setOptionsOn(!optionsOn)}>
-              <i className="fas fa-shopping-bag" />
-            </button>
-        </div>
         <div className='nav_options'>
-          {user ? null :
-            <>
-            <>
+          {user ? <>
+            <button className='nav_sidebar_icons nav_cart_icon'>
+              <i className="fas fa-shopping-cart" />
+            </button>
+          </>
+          : <>
             <NavLink to='/login' exact={true} activeClassName='active'>
               Login
             </NavLink>
-            </>
-            <>
             <NavLink to='/sign-up' exact={true} activeClassName='active'>
               Sign Up
             </NavLink>
-            </>
             </>
           }
         </div>
       </div>
     </nav>
     <div className="sidebar" style={!optionsOn ? {transform: 'translateX(-110%)'} : {}}>
-      <div>
-        <button className="arrow-button" onClick={()=> setOptionsOn(!optionsOn)}>
+      <div className='sidebar_container'>
+        <div className="arrow-button" onClick={()=> setOptionsOn(!optionsOn)}>
           <i className="fas fa-arrow-left"></i>
-        </button>
-      </div>
-      <div>
-        <div>
-          {user ? user.username : 'log In!'}
         </div>
-        <div>
-          {user? <LogoutButton/> : 'more features coming soon!'}
+        <div className='sidebar_userInfo'>
+          <div>
+            <p>
+            {user ? 'Welcome  ' + user.username + '!' : 'Please log In'}
+            </p>
+            <p>
+            {user ? 'Email:  ' + user.email : 'Or sign up to see more!'}
+            </p>
+          </div>
+          <div>
+            {user? <LogoutButton/> : 'More features coming soon!'}
+          </div>
         </div>
       </div>
     </div>
