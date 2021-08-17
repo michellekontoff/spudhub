042fb144e61cae2a5405f571d53351b34104906f
steffano <steffano.espinoza10@gmail.com>
2021-08-17 10:44:51 -0700

    in progress of adding an options tab bar

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,5 +1,6 @@
 
 import React from 'react';
+import { useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import LogoutButton from './auth/LogoutButton';
@@ -8,14 +9,21 @@ import './NavBar.css'
 const NavBar = () => {
 
   const user = useSelector(state => state.session.user);
+  const [optionsOn, setOptionsOn] = useState(false)
 
   return (
+    <>
     <nav>
       <div className='nav_container'>
         <div className='nav_logo'>
           <NavLink to='/' exact={true} activeClassName='active'>
             SpudHub
           </NavLink>
+        </div>
+        <div>
+            <button onClick={()=> setOptionsOn(!optionsOn)}>
+              <i className="fas fa-shopping-bag" />
+            </button>
         </div>
         <div className='nav_options'>
           {user ? <LogoutButton/> :
@@ -35,27 +43,31 @@ const NavBar = () => {
         </div>
       </div>
     </nav>
+    {optionsOn ? <OptionsTab/> : null}
+    </>
   );
 }
 
+function OptionsTab(){
+  const [showOptions, setShowOptions] = useState(true);
+
+  return (
+    <>
+    <div style={showOptions ? { marginLeft: '300px' } : {}} >
+      <p>name</p>
+      <p>logout</p>
+    </div>
+    <div
+      style={showOptions ? { transform: 'translateX(-100%)' } : {}}
+    >
+      <div className='test'>
+      <button className="arrow-button" onClick={() => setShowOptions(false)}>
+        <i className="fas fa-arrow-right"></i> hide
+      </button>
+      </div>
+    </div>
+    </>
+  )
+}
+
 export default NavBar;
-
-
-
-
-
-{/* <li>
-  <NavLink to='/login' exact={true} activeClassName='active'>
-    Login
-  </NavLink>
-</li>
-<li>
-  <NavLink to='/sign-up' exact={true} activeClassName='active'>
-    Sign Up
-  </NavLink>
-</li> */}
-{/* <li>
-  <NavLink to='/users' exact={true} activeClassName='active'>
-    Users
-  </NavLink>
-</li> */}
