a7bf7a84413ebc503b51a66db789bdda2ff20d8e
steffano <steffano.espinoza10@gmail.com>
2021-08-17 09:54:12 -0700

    added styling to navbar

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,38 +1,61 @@
 
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
 import LogoutButton from './auth/LogoutButton';
+import './NavBar.css'
 
 const NavBar = () => {
+
+  const user = useSelector(state => state.session.user);
+
   return (
     <nav>
-      <ul>
-        <li>
+      <div className='nav_container'>
+        <div className='nav_logo'>
           <NavLink to='/' exact={true} activeClassName='active'>
-            Home
+            SpudHub
           </NavLink>
-        </li>
-        <li>
-          <NavLink to='/login' exact={true} activeClassName='active'>
-            Login
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/sign-up' exact={true} activeClassName='active'>
-            Sign Up
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/users' exact={true} activeClassName='active'>
-            Users
-          </NavLink>
-        </li>
-        <li>
-          <LogoutButton />
-        </li>
-      </ul>
+        </div>
+        <div className='nav_options'>
+          {user ? <LogoutButton/> :
+            <>
+            <>
+            <NavLink to='/login' exact={true} activeClassName='active'>
+              Login
+            </NavLink>
+            </>
+            <>
+            <NavLink to='/sign-up' exact={true} activeClassName='active'>
+              Sign Up
+            </NavLink>
+            </>
+            </>
+          }
+        </div>
+      </div>
     </nav>
   );
 }
 
 export default NavBar;
+
+
+
+
+
+{/* <li>
+  <NavLink to='/login' exact={true} activeClassName='active'>
+    Login
+  </NavLink>
+</li>
+<li>
+  <NavLink to='/sign-up' exact={true} activeClassName='active'>
+    Sign Up
+  </NavLink>
+</li> */}
+{/* <li>
+  <NavLink to='/users' exact={true} activeClassName='active'>
+    Users
+  </NavLink>
+</li> */}
