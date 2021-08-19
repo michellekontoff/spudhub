5df0b01089cce5bb57f084f211680b52c76f9c24
Manna Kong <m.kong.57@gmail.com>
2021-08-19 14:47:14 -0500

    done with unique name edits

--- app/forms/product_edit_form.py
+++ app/forms/product_edit_form.py
@@ -8,20 +8,16 @@ from app.models import Product
 def check_name_and_length(form, field):
     name = field.data
 
-    products = Product.query.filter(Product.name == name and Product.id != form.data['product_id']).all()
-    print('----------------')
-    print(products)
-    print(form.data['product_id'])
+    product = Product.query.filter(Product.name == name).first()
 
-    if 2 < len(name) > 50 or :
-        raise ValidationError("Name must be between 2 and 50 characters.")
-
+    if product and product.id != form.data['product_id']:
+        raise ValidationError("Name must be unique.")
 
 
 
 class ProductEditForm(FlaskForm):
     product_id = IntegerField("product_id")
-    name = StringField('Name', validators=[DataRequired(), check_name_and_length])
+    name = StringField('Name', validators=[DataRequired(), Length(min=2, max=50, message="Name must be between %(min)d and %(max)d characters."), check_name_and_length])
     description = TextAreaField('Description', validators=[DataRequired(), Length(min=2, max=255, message="Description must be between %(min)d and %(max)d characters.")])
     price = DecimalField('Price', validators=[DataRequired(), NumberRange(min=0.01 ,max=99999999.99, message="Price must be between $%(min)d and $%(max)d.")])
     quantity = IntegerField('Quantity',validators=[DataRequired(), NumberRange(min=1, message="Quantity must be at least %(min)d.")])
