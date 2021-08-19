c70490a2d97d49aa14f45d4f4048928f5f6d1a32
Manna Kong <m.kong.57@gmail.com>
2021-08-18 19:15:09 -0500

    product page

--- react-app/src/components/Products/ProductDetails.js
+++ react-app/src/components/Products/ProductDetails.js
@@ -17,8 +17,7 @@ const ProductDetails = ({userId, product, editMode, setEditMode}) =>{
             <h1 className='product-header'>{product.name}</h1>
             <div className="product-img-container"><img src={product.image} alt={product.name}></img></div>
             <div className='product-description'>{product.description}</div>
-            <div className='product-price'>{product.price}</div>
-            <div className='product-quantity'>{product.quantity}</div>
+            <div className='product-price'>${product.price}</div>
             <div className='product-edit-btn'>{editButton}</div>
         </div>
 
