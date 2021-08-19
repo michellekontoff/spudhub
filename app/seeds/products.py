5511c1831a8424ee62c79776217c80d10890d890
Manna Kong <m.kong.57@gmail.com>
2021-08-19 14:52:11 -0500

    eatten to eaten

--- app/seeds/products.py
+++ app/seeds/products.py
@@ -16,7 +16,7 @@ def seed_products():
         p = Product(
             user_id = random.randint(1, 3),
             name=f'{ j_name.title() }',
-            description=f'This is a locally grown { j_name }. It\'s fresh and ready to be eatten',
+            description=f'This is a locally grown { j_name }. It\'s fresh and ready to be eaten.',
             price=float(random.randint(1, 20)) + 0.99,
             image=(f'https://raw.githubusercontent.com/michellekontoff/spudhub/main/product_images/{ name }.jpg'),
             created_at=datetime.now(),
